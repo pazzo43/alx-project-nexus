1c70c1a52f242a380869666c9e11b5fa24b9cf61
Patrick NDICUNGUYE <pazzondicunguye43@gmail.com>
2026-02-02 21:39:20 +0200

    Update App.tsx

--- nexus-prodev-fe/App.tsx
+++ nexus-prodev-fe/App.tsx
@@ -13,3 +13,20 @@ npm start
 git init
 git add .
 git commit -m "feat: initialize Expo project with TypeScript"
+
+
+import { Provider } from "react-redux";
+import { store } from "./src/store";
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Project Nexus – ProDev Frontend</Text>
+      </View>
+    </Provider>
+  );
+}
+
+
