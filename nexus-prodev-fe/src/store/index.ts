d702c291092d9679a63b6eb58b7ca4e58422bf80
Patrick NDICUNGUYE <pazzondicunguye43@gmail.com>
2026-02-02 21:26:51 +0200

    Update index.ts

--- nexus-prodev-fe/src/store/index.ts
+++ nexus-prodev-fe/src/store/index.ts
@@ -1,1 +1,15 @@
+npm install @reduxjs/toolkit react-redux
 
+import { configureStore } from '@reduxjs/toolkit';
+import { productsApi } from './services/productsApi';
+
+export const store = configureStore({
+  reducer: {
+    [productsApi.reducerPath]: productsApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(productsApi.middleware),
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
