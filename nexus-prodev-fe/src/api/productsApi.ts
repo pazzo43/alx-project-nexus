8f1e442401e3834a3ce249ab503dff318e74149b
Patrick NDICUNGUYE <pazzondicunguye43@gmail.com>
2026-02-02 21:44:29 +0200

    Update productsApi.ts

--- nexus-prodev-fe/src/api/productsApi.ts
+++ nexus-prodev-fe/src/api/productsApi.ts
@@ -1,1 +1,13 @@
+import { apiSlice } from "./apiSlice";
+import { Product } from "../types/product";
+
+export const productsApi = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getProducts: builder.query<Product[], void>({
+      query: () => "products/",
+    }),
+  }),
+});
+
+export const { useGetProductsQuery } = productsApi;
 
