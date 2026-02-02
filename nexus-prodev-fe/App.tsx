b56cc5200b9a2414d46716b44e29329b6c711d7f
Patrick NDICUNGUYE <pazzondicunguye43@gmail.com>
2026-02-02 21:12:45 +0200

    Update App.tsx

--- nexus-prodev-fe/App.tsx
+++ nexus-prodev-fe/App.tsx
@@ -1,1 +1,10 @@
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Project Nexus – ProDev Frontend</Text>
+    </View>
+  );
+}
 
