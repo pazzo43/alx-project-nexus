25b0a46b84c0738aa1c6a9aac79f9fdcce481d79
Patrick NDICUNGUYE <pazzondicunguye43@gmail.com>
2026-02-03 07:34:51 +0200

    Update ProductListScreen.tsx

--- nexus-prodev-fe/src/screens/ProductListScreen.tsx
+++ nexus-prodev-fe/src/screens/ProductListScreen.tsx
@@ -85,3 +85,8 @@ const [sortOrder, setSortOrder] = useState<"price" | "-price">("price");
   )}
 />
 
+git add .
+git commit -m "feat: add category filtering to product list"
+git commit -m "feat: implement price sorting using API params"
+git commit -m "refactor: connect filters to RTK Query"
+
