cdc2500d8ab70f73b81e885b2c6fecde5bad726f
Patrick NDICUNGUYE <pazzondicunguye43@gmail.com>
2026-02-03 07:29:56 +0200

    Update CategoryFilter.tsx

--- nexus-prodev-fe/components/CategoryFilter.tsx
+++ nexus-prodev-fe/components/CategoryFilter.tsx
@@ -1,1 +1,10 @@
+<View style={{ flexDirection: "row" }}>
+  {[1, 2, 3].map((catId) => (
+    <Button
+      key={catId}
+      title={`Category ${catId}`}
+      onPress={() => setSelectedCategory(catId)}
+    />
+  ))}
+</View>
 
