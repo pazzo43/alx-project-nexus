9d9f4b0755dfc8384357979bd3781d8c05528275
Patrick NDICUNGUYE <pazzondicunguye43@gmail.com>
2026-02-03 06:56:01 +0200

    Update store.ts

--- nexus-prodev-fe/src/app/store.ts
+++ nexus-prodev-fe/src/app/store.ts
@@ -1,1 +1,15 @@
+// src/app/store.ts
+import { configureStore } from "@reduxjs/toolkit";
+import { api } from "../services/api";
+
+export const store = configureStore({
+  reducer: {
+    [api.reducerPath]: api.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
 
